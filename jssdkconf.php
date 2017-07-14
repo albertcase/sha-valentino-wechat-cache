418e80bc959221c753b0e60aebd704f27eada654
ke.wang@samesamechina.com <ke.wang@samesamechina.com>
2017-07-14 12:21:41 +0800

    en:edit dirc jssdk cache code

---
+++ jssdkconf.php
@@ -0,0 +1,29 @@
+<?php
+	return array(
+		'appid' => '',
+		'secret' => '',
+		'redis_host' => '10.10.144.175',
+		'redis_port' => '6379',
+		'redis_prefix' => '',
+		'jssdk_id_list' => array(
+			 	'65454635-a701-4ea4-b7f5-5cebae7e6674',
+			 	'60c4349e-c302-4313-9fa8-37a8ebd59853',
+			),
+		'jssdk_api_list' => array(
+				'checkJsApi',
+	      		'onMenuShareTimeline',
+	      		'onMenuShareAppMessage',
+	      		'onMenuShareQQ',
+	      		'onMenuShareWeibo',
+	      		'hideMenuItems',
+	      		'showMenuItems',
+	      		'hideAllNonBaseMenuItem',
+	      		'showAllNonBaseMenuItem',
+	      		'getNetworkType',
+	      		'openLocation',
+	      		'getLocation',
+	      		'hideOptionMenu',
+	      		'showOptionMenu',
+	      		'closeWindow',
+			),
+	);
